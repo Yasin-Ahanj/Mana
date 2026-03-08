ce4c7bef7573368c75c8286347aa9e723150d0ed
Yasin-Ahanj <yassin.ahanj@gmail.com>
2026-03-08 19:07:25 +0330

    saved

--- app/Pages/Nameofperson/page.tsx
+++ app/Pages/Nameofperson/page.tsx
@@ -13,6 +13,7 @@ const Nameofperson = () =>{
             <h1 className="flex justify-end w-[95%]"> ثبت نام اشخاص به دوره </h1>
             <TextInput containerStyle="flex flex-col gap-1 items-end w-[90%] mx-auto mt-4" placeholder="نام و نام خانوادگی " style="w-full  border text-right p-4 rounded-[12px]" labelTxt="نام و نام خانوادگی " name="name" onChange={(e) => setName(e.target.value)} value={name} />
             <TextInput containerStyle="flex flex-col gap-1 items-end w-[90%] mx-auto mt-4" placeholder="2900449081" style="w-full border text-right p-4 rounded-[12px]" labelTxt="کدملی" name="phone" onChange={(e) => setMobile(e.target.value)} value={mobile} />   
+            <TextInput containerStyle="flex flex-col gap-1 items-end w-[90%] mx-auto mt-4" placeholder="2900449081" style="w-full border text-right p-4 rounded-[12px]" labelTxt="کدملی" name="phone" onChange={(e) => setMobile(e.target.value)} value={mobile} />   
         </div>
     )
 
